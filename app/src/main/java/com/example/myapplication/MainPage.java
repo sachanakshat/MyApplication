064d388f44f8447e90a21f17eb5b8bb445c9ad98
Akshat Sachan <TheRedHatCoder@localhost.localdomain>
2019-05-29 15:42:00 +0530

    BNB crash, System Camera added, and basic fragment layout provided

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -1,52 +1,62 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.Toast;
+import android.support.v4.app.Fragment;
+
 
 public class MainPage extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-//                case R.id.camera_icon:
-//                    Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
-//                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-//                    startActivity(intent);
-//                    return true;
-                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-                    Toast.makeText(MainPage.this, "Home", Toast.LENGTH_SHORT).show();
-                    return true;
-//                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-                    Toast.makeText(MainPage.this, "Notifications", Toast.LENGTH_SHORT).show();
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main_page2);
 
-//        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        Fragment fragment;
+
+        BottomNavigationView navigation2 = (BottomNavigationView) findViewById(R.id.navigationxml);
+        navigation2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.camera_icon:
+                        Toast.makeText(MainPage.this, "The next best feature coming", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent("android.media.action.IMAGE_CAPTURE"));
+                        break;
+                    case R.id.messages_icon:
+                        Toast.makeText(MainPage.this, "Check back later!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_home:
+                        Toast.makeText(MainPage.this, "You are FragHome!", Toast.LENGTH_SHORT).show();
+                        Fragment fragment = new FragHome();
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.replace(R.id.frag_container, fragment);
+                        ft.commit();
+                        break;
+                    case R.id.navigation_notifications:
+                        Toast.makeText(MainPage.this, "No new notifications", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_account:
+                        Toast.makeText(MainPage.this, "Under development", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
     }
 
+//    @Override
+//    public void ChangeFragment(View view) {
+//
+//    }
 }
--- app/src/main/java/com/example/myapplication/StartPage.java
+++ app/src/main/java/com/example/myapplication/StartPage.java
@@ -1,18 +1,16 @@
 package com.example.myapplication;
 
-import android.app.NotificationChannel;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+public class StartPage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_page);
     }
 
 //    @Override
