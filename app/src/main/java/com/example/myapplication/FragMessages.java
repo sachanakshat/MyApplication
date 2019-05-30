755853eb4d5c2b856816a5d0fe07a27be862c320
Akshat Sachan <TheRedHatCoder@localhost.localdomain>
2019-05-30 16:56:35 +0530

    Fragments initialized

--- app/src/main/java/com/example/myapplication/FragMessages.java
+++ app/src/main/java/com/example/myapplication/FragMessages.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FragAccount.OnFragmentInteractionListener} interface
+ * {@link FragMessages.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link FragAccount#newInstance} factory method to
+ * Use the {@link FragMessages#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragAccount extends Fragment {
+public class FragMessages extends Fragment {
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class FragAccount extends Fragment {
 //
 //    private OnFragmentInteractionListener mListener;
 //
-//    public FragAccount() {
+//    public FragMessages() {
 //        // Required empty public constructor
 //    }
 //
@@ -39,11 +39,11 @@ public class FragAccount extends Fragment {
 //     *
 //     * @param param1 Parameter 1.
 //     * @param param2 Parameter 2.
-//     * @return A new instance of fragment FragAccount.
+//     * @return A new instance of fragment FragMessages.
 //     */
 //    // TODO: Rename and change types and number of parameters
-//    public static FragAccount newInstance(String param1, String param2) {
-//        FragAccount fragment = new FragAccount();
+//    public static FragMessages newInstance(String param1, String param2) {
+//        FragMessages fragment = new FragMessages();
 //        Bundle args = new Bundle();
 //        args.putString(ARG_PARAM1, param1);
 //        args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class FragAccount extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return inflater.inflate(R.layout.fragment_messages, container, false);
     }
 
 //    // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -20,12 +20,17 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page2);
 
-//        Fragment fragment;
+        loadFragment(new FragHome());
+
+//        Fragment fragment = null;
+//        FragmentManager fm = getSupportFragmentManager();
+//        final FragmentTransaction ft = fm.beginTransaction();
 
         BottomNavigationView navigation2 = (BottomNavigationView) findViewById(R.id.navigationxml);
         navigation2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
                 switch (item.getItemId()) {
                     case R.id.camera_icon:
                         Toast.makeText(MainPage.this, "The next best feature coming", Toast.LENGTH_SHORT).show();
@@ -33,30 +38,38 @@ public class MainPage extends AppCompatActivity {
                         break;
                     case R.id.messages_icon:
                         Toast.makeText(MainPage.this, "Check back later!", Toast.LENGTH_SHORT).show();
+                        fragment = new FragMessages();
                         break;
                     case R.id.navigation_home:
                         Toast.makeText(MainPage.this, "You are FragHome!", Toast.LENGTH_SHORT).show();
-                        Fragment fragment = new FragHome();
-                        FragmentManager fm = getSupportFragmentManager();
-                        FragmentTransaction ft = fm.beginTransaction();
-                        ft.replace(R.id.frag_container, fragment);
-                        ft.commit();
+                        fragment = new FragHome();
+//                        ft.replace(R.id.frag_container, new FragHome());
+//                        ft.commit();
                         break;
                     case R.id.navigation_notifications:
                         Toast.makeText(MainPage.this, "No new notifications", Toast.LENGTH_SHORT).show();
+                        fragment = new FragNotifs();
                         break;
                     case R.id.navigation_account:
                         Toast.makeText(MainPage.this, "Under development", Toast.LENGTH_SHORT).show();
+                        fragment = new FragAccount();
                         break;
 
                 }
-                return true;
+                return loadFragment(fragment);
             }
         });
     }
 
-//    @Override
-//    public void ChangeFragment(View view) {
-//
-//    }
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frag_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
